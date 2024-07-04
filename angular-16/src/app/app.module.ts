d21c2ce920a290de8fcb6bbd050c4b9287754898
Rudi Antoni <rudiantoni2013@gmail.com>
2024-07-04 08:14:06 -0300

    updated angular 16

--- angular-16/src/app/app.module.ts
+++ angular-16/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    ButtonModule,
+    DropdownModule,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NgModel],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-16/src/app/components/home/home.component.ts
+++ angular-16/src/app/components/home/home.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  cities: City[] = [];
+  selectedCity?: City = undefined;
 
+  ngOnInit() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' },
+    ];
+  }
+
+  selectCustom(): void {
+    this.selectedCity = this.cities[0];
+  }
+
+  printCurrent(): void {
+    console.log('this.selectedCity', this.selectedCity);
+  }
 }
